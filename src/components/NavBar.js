d65ef9d3809138ba7a16b44fe81fec2d3ef2b488
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-05-30 17:50:47 -0400

    Nav Bar Links work now

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import { useState } from "react";
+import { Navbar, Nav } from 'react-bootstrap'
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
-  return (
-	<div>
-		<h2>Haha</h2>
-	</div>
-  )
+	const [activeLink, setActiveLink] = useState("home");
+	const navigate = useNavigate();
+
+	const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
+
+	return (
+		<div className='nav-bar'>
+			<Navbar expand="lg">
+				<Navbar.Toggle aria-controls="basic-navbar-nav">
+					<Nav className='nav-bar-links'>
+						<Nav.Link to="/about" className={activeLink === 'about' ? "active navbar-link" : "navbar-link"} onClick={() => {onUpdateActiveLink('about'); navigate('/about')}}>About</Nav.Link>
+						<Nav.Link to="/shop" className={activeLink === 'shop' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('shop'); navigate('/shop')}}>Shop</Nav.Link>
+						<Nav.Link to="/request" className={activeLink === 'request' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('request'); navigate('/request')}}>Request</Nav.Link>
+					</Nav>
+				</Navbar.Toggle>
+			</Navbar>
+		</div>
+	)
 }
 
 export default NavBar
