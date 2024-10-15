2869191db247b4d50f1a3f6361293c40b628050f
edwardwang4340 <eddywang530@gmail.com>
2024-10-14 20:08:32 -0400

    updated printer.js

--- src/components/About/Printer.js
+++ src/components/About/Printer.js
@@ -4,13 +4,14 @@ import '../../styling/About/printer.css'
 
 import Geetech from '../../assets/other/geetech.png'
 import Monoprice from '../../assets/other/monoprice.png'
+import Bambulab from '../../assets/other/bambu_lab.png'
 
 const Printer = () => {
   return (
 	<div className='printer'>
 		<h2><strong>Our Printers</strong></h2>
 		<Container className='d-flex flex-column align-items-center justify-content-center'>
-			<Row className='printer-section-one align-items-center justify-content-center'>
+			{/* <Row className='printer-section-one align-items-center justify-content-center'>
 				<Col xl={4} md={8} xs={8} className='printer-image'> 
 					<img src={Geetech} alt="Geetech A10M"/>
 				</Col>
@@ -28,6 +29,16 @@ const Printer = () => {
 					<h3 className>Monoprice Voxel</h3>
 					<hr></hr>
 					<p>The Monoprice Voxel has multiple features and is great for beginners. It is fully enclosed with glass panels, printing with minimal noise. It has a wireless printing option and produces high quality prints. When receiving the package, it is already assembled for you and is ready to print once package opened. </p>
+				</Col>
+			</Row> */}
+			<Row className='printer-section-two align-items-center justify-content-center'>
+				<Col xl={4} md={8} xs={8} className='printer-image'>
+					<img src={Bambulab} alt="Bambu Lab"/>
+				</Col>
+				<Col xl={5} md={8} xs={8} className='printer-description'>
+					<h3 className>BambuLab X1 Carbon with AMS</h3>
+					<hr></hr>
+					<p>The BambuLab X1 Carbon with AMS is a powerful 3D printer designed for professionals and enthusiasts. With the AMS (Automatic Material System), you can easily switch between multiple filament types and colors. The X1 Carbon offers wireless printing, and its fast printing speeds don’t compromise on quality. This printer excels in delivering high-quality, detailed prints. </p>
 				</Col>
 			</Row>
 		</Container>
